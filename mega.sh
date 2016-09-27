40da376bba7a77e9aa900ea3f290e5847b55adbd
Heorhi Sidoryn <heorhi.sidoryn@gmail.com>
2016-09-27 19:35:27 +0300

    added env.sample, replace sh to bash command

--- mega.sh
+++ mega.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sh ./gen-and-dep-keys.sh
-sh ./install-controller.sh
-sh ./install-worker.sh
-sh ./register-keys.sh
+source ./env
+
+bash ./gen-and-dep-keys.sh
+bash ./install-controller.sh
+bash ./install-worker.sh
+bash ./register-keys.sh
